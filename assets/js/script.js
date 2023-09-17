423e76b50901b84b26a029921bacb3f546f84003
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-09-17 13:15:31 +0330

    Empty validation works

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,30 @@ const resultMonth = document.querySelector("#result-month");
 const resultDay = document.querySelector("#result-day");
 
 // Validation
+
+function validate(e) {
+  const target = e.target;
+  const targetValue = target.value;
+  const targetId = target.id;
+
+  // target id is in fact a sibling of err-msg FIX THESE TWO
+  const errMsg = target.nextSibling.nextSibling;
+  const errMsgId = errMsg.getAttribute("id");
+  const emptyMsg = document.querySelector(`#${errMsgId} .empty-msg`);
+  const invalidMsg = document.querySelector(`#${errMsgId} .invalid-msg`);
+
+  // Check if target input is empty
+  if (targetValue == "") {
+    if (!target.classList.contains("invalid")) {
+      target.classList.add("invalid");
+      emptyMsg.classList.add("visible");
+    }
+  } else {
+    target.classList.remove("invalid");
+    emptyMsg.classList.remove("visible");
+  }
+}
+
+dayInput.addEventListener("change", validate);
+monthInput.addEventListener("change", validate);
+yearInput.addEventListener("change", validate);
