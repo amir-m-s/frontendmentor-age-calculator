c72a652ad6ff4a6abfb887d4e5471d218164b2b8
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-09-17 12:11:57 +0330

    initialize script

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Input Variables
+const dayInput = document.querySelector("#day-input");
+const monthInput = document.querySelector("#month-input");
+const yearInput = document.querySelector("#year-input");
+
+// Calculate Button
+const calculateBtn = document.querySelector(".calculate-btn");
+
+// Result Variables
+const resultYear = document.querySelector("#result-year");
+const resultMonth = document.querySelector("#result-month");
+const resultDay = document.querySelector("#result-day");
+
+console.log(dayInput, monthInput, yearInput, calculateBtn, resultDay, resultMonth, resultYear);
