6c4296d03881f0f25019139f4916c269217ee1bb
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-09-17 13:36:34 +0330

    Error messages completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Current Date
+const currentDate = new Date();
+
 // Input Variables
 const dayInput = document.querySelector("#day-input");
 const monthInput = document.querySelector("#month-input");
@@ -15,16 +18,15 @@ const resultDay = document.querySelector("#result-day");
 
 // Validation
 
+// Validate function on change
 function validate(e) {
   const target = e.target;
   const targetValue = target.value;
-  const targetId = target.id;
 
   // target id is in fact a sibling of err-msg FIX THESE TWO
   const errMsg = target.nextSibling.nextSibling;
   const errMsgId = errMsg.getAttribute("id");
   const emptyMsg = document.querySelector(`#${errMsgId} .empty-msg`);
-  const invalidMsg = document.querySelector(`#${errMsgId} .invalid-msg`);
 
   // Check if target input is empty
   if (targetValue == "") {
@@ -36,6 +38,37 @@ function validate(e) {
   }
 }
 
+function dmValidation(e) {
+  const target = e.target;
+  const targetValue = target.value;
+
+  // target id is in fact a sibling of err-msg FIX THESE TWO
+  const errMsg = target.nextSibling.nextSibling;
+  const errMsgId = errMsg.getAttribute("id");
+  const invalidMsg = document.querySelector(`#${errMsgId} .invalid-msg`);
+
+  // Check if target input is empty
+  if (errMsgId === "day-msg" && targetValue > 31) {
+    target.classList.add("invalid");
+    invalidMsg.classList.add("visible");
+  } else if (errMsgId === "month-msg" &&  targetValue > 12) {
+    target.classList.add("invalid");
+    invalidMsg.classList.add("visible");
+    console.log(currentDate.getFullYear());
+  } else if (errMsgId === "year-msg" && Number(targetValue) > Number(currentDate.getFullYear())) {
+    target.classList.add("invalid");
+    invalidMsg.classList.add("visible");
+  } else {
+    target.classList.remove("invalid");
+    invalidMsg.classList.remove("visible");
+  }
+  console.log(errMsgId);
+}
+
 dayInput.addEventListener("change", validate);
 monthInput.addEventListener("change", validate);
 yearInput.addEventListener("change", validate);
+
+dayInput.addEventListener("change", dmValidation);
+monthInput.addEventListener("change", dmValidation);
+yearInput.addEventListener("change", dmValidation);
