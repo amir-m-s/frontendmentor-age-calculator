4c5c7973e4e3946a28f6f3cb040d5c93aeab12e7
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-09-20 17:35:04 +0330

    Update script.js
    Calculation logic is addd and working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,11 +51,14 @@ function dmValidation(e) {
   if (errMsgId === "day-msg" && targetValue > 31) {
     target.classList.add("invalid");
     invalidMsg.classList.add("visible");
-  } else if (errMsgId === "month-msg" &&  targetValue > 12) {
+  } else if (errMsgId === "month-msg" && targetValue > 12) {
     target.classList.add("invalid");
     invalidMsg.classList.add("visible");
     console.log(currentDate.getFullYear());
-  } else if (errMsgId === "year-msg" && Number(targetValue) > Number(currentDate.getFullYear())) {
+  } else if (
+    errMsgId === "year-msg" &&
+    Number(targetValue) > Number(currentDate.getFullYear())
+  ) {
     target.classList.add("invalid");
     invalidMsg.classList.add("visible");
   } else {
@@ -65,8 +68,7 @@ function dmValidation(e) {
   console.log(errMsgId);
 }
 
-
-// Add Event Listeners
+// Add Event Listeners To validate
 dayInput.addEventListener("change", validate);
 monthInput.addEventListener("change", validate);
 yearInput.addEventListener("change", validate);
@@ -74,3 +76,41 @@ yearInput.addEventListener("change", validate);
 dayInput.addEventListener("change", dmValidation);
 monthInput.addEventListener("change", dmValidation);
 yearInput.addEventListener("change", dmValidation);
+
+// Calculation function
+function calculateAge() {
+  let date = `${yearInput.value}-${monthInput.value}-${dayInput.value}`;
+  const ageDateFormatted = new Date(date);
+
+  // Get difference in milliseconds
+  const dateDifferenceInMilliseconds = currentDate - ageDateFormatted;
+
+  // Get results in year, month and day
+  let resultYear = Math.floor(dateDifferenceInMilliseconds * 3.171e-11);
+  let resultMonth = Math.floor(
+    (dateDifferenceInMilliseconds * 3.80517e-10) % 12
+  );
+  let resultDay = Math.floor((dateDifferenceInMilliseconds * 1.15741e-8) % 36);
+  
+  // Calendar logic
+  if (resultDay > 31) {
+    resultMonth += 1;
+    resultDay -= 31;
+  }
+  if (resultMonth > 12) {
+    resultYear += 1;
+    resultMonth -= 12;
+  }
+  // Show results to user using show function
+  show(resultYear, resultMonth, resultDay);
+}
+
+// Put information on placeholders
+function show(year, month, day) {
+  resultYear.innerText = year;
+  resultMonth.innerText = month;
+  resultDay.innerText = day;
+}
+
+// Add Event Listener to Calculate Age
+calculateBtn.addEventListener("click", calculateAge);
